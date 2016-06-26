4a238cd02a40ee23bb5926a390d1cb20480bbed8
Grant Willey <email": null>
2016-06-26 11:55:01 -0500

    long and lat based stories

--- app/assets/javascripts/stories/stories.js
+++ app/assets/javascripts/stories/stories.js
@@ -30,25 +30,52 @@ Story.prototype.coherentGrammar = function() { return {
   "expandedOrigin": ["It feels like just yester#timeframe# that I was flying past #spaceThing#.", "It wasn't an accident... it was SABOTAGE!", `There are other ${this.meteorite.recclass} meteorites just like me who survived... I am sure of it.`],
   "relative": ["my cousin", "my brother", "my sister", "my neighbor", "my dear friend"],
   "hint": [`along ${this.meteorite.nextMeteorite.getLat()} latitude`, `along ${this.meteorite.nextMeteorite.getLong()} longitude`, `in ${this.meteorite.nextMeteorite.getYear()} AD`],
-  "tropical": ["OMG A LION, GET IN THE CAR", "¿Qué Paso?", "I was approaching Earth, aimed straight for the middle, and... BULLSEYE!"],
-  "ice:(": ["Which one has penguins and which one has polar bears?  I always forget.", "Are you Santa?", "Brrrrrr!"],
-  "temperate": ["What a rainy day.", "Yay seasons!", "DID BIGFOOT JUST WALK BY!"],
+  "africa": ["OMG A LION, GET IN THE CAR", "Pyramids!", "I was approaching Earth, aimed straight for the middle, and... BULLSEYE!"],
+  "arctic": ["My compass is broken.", "Are you Santa?", "Where is everybody?"],
+  "antarctica": ["Brrrrrr!", "Which one has penguins and which one has polar bears?  I always forget."],
+  "north-america": ["What a rainy day.", "Yay seasons!", "DID BIGFOOT JUST WALK BY!"],
+  "south-america": ["¿Qué Paso?", "I've a distant cousin who is a soccer ball!", "I could go for some Yerba Mate."],
+  "europe": ["I can't keep track of all these kings and queens.", "Pasta!", "These buildings look older than me!"],
+  "australia": ["G'day Mate.", "Blimey!", "What's the deal with the platypus, I mean, really?"],
+  "asia": ["I can see that wall from home!", "Oh no, Mongol hordes!", "Fireworks!"],
   "clue": ["Please find #relative#!  Look #hint#."],
-  "origin": [`#thanks# #expandedOrigin# #${this.whereaboutsLat(this.meteorite.getLat())}# #clue#`]
+  "origin": [`#thanks# #expandedOrigin# #${this.whereabouts(this.meteorite)}# #clue#`]
   }
 }
 
-Story.prototype.whereaboutsLat = function(latitude) {
-  if (latitude > 60 || latitude < -60) {
-    return "ice:("
-  }
-  else if (latitude <= 20 && latitude >= -20) {
-    return "tropical"
+Story.prototype.whereabouts = function(meteorite) {
+  var latitude = meteorite.getLat();
+  var longitude = meteorite.getLong();
+
+  if (latitude > 60) {
+    return "arctic";
+  } else if (latitude < -60) {
+    return "antarctica";
+  } else if (longitude < -30 && latitude > 0) {
+    return "north-america";
+  } else if (longitude < -30 && latitude < 0) {
+    return "south-america";
+  } else if (latitude < 32 && longitude < 45) {
+    return "africa";
+  } else if (latitude > 32 && longitude < 30) {
+    return "europe";
+  } else if (latitude > 90 && longitude < 10) {
+    return "australia";
   } else {
-    return "temperate"
+    return "asia";
   }
-
 }
+
+// Story.prototype.whereaboutsClimate = function(meteorite) {
+//   if (latitude > 60 || latitude < -60) {
+//     return "ice:("
+//   }
+//   else if (latitude <= 20 && latitude >= -20) {
+//     return "tropical"
+//   } else {
+//     return "temperate"
+//   }
+// }
 
 
 // var warStory = {
