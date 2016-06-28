9984f0867bb56e32b9290a66116b1bc8c75c3828
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-28 09:55:28 -0500

    Complete sessions login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    render :layout => false
   end
 
   def create
-    user = User.find_by(email: params[:sessions][:email])
+    user = User.find_by(username: params[:sessions][:username])
     if user && user.authenticate(params[:sessions][:password])
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now.alert = "Invalid username or password"
       render "new"
     end
   end
