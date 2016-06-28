c1d4850cc69d364be438d51fa167fea1351246eb
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-27 20:42:08 -0500

    Create controller, routes, and views for creating new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  # before_action :find_user, only: [:new, :create]
+def new
+  @user = User.new
+  render :layout => false
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to '/'
+  else
+    render "new"
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+
+  resources :users
+  # get 'users/new'
+  # get 'users/create'
+  # post 'users'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
