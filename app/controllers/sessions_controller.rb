3d01275317db8e0adbc164bc8c059e88e43c7a13
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-28 15:07:20 -0500

    Fix login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:sessions][:username])
     if user && user.authenticate(params[:sessions][:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to user_path(id: user.id)
     else
       flash.now.alert = "Meteor-WRONG! username or password"
       render "new"
