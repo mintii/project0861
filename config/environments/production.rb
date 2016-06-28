d39b73af75276b57975ad5870a92b6c7d262d626
dbravman <dbravman@gmail.com>
2016-06-28 16:36:55 -0500

    post - duke

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.assets.precompile += %w[ cartodb/*.css cartodb/*.js ]
+  config.assets.precompile += %w[ cartodb/*.js ]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
