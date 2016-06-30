47f198e9087371afb78988d23a2acbaedecceebf
Tim Lantz <tlantz@gmail.com>
2016-06-30 16:49:28 -0500

    change button to say unlock it memories!

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -168,7 +168,7 @@ var renderInfo = function(meteorite) {
   renderProfileImage(meteorite);
 
   if (!meteorite.defeated) {
-  $('#minigame-buttons').html("<button class='minigame-buttons' id='minigame-button'>Play Minigame!</button>");
+  $('#minigame-buttons').html("<button class='minigame-buttons' id='minigame-button' style='border-radius: 2px'>Unlock it's Memories!</button>");
   } else {$('#minigame-buttons').html(""); };
 }
 
@@ -188,8 +188,3 @@ var renderProfileImage = function(meteorite) {
 }
 
 }
-
-
-
-
-
