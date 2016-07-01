c1a3ee4f33a54d5ef364019e0370e341f8ab37e3
Brittney Braxton <mintii@users.noreply.github.com>
2016-07-01 07:58:37 -0500

    Trying to serve assets...

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Project0861
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.serve_static_assets = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
